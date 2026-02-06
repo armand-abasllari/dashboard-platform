ec810cdc77a9f893354ba60e8d922bda7fab7eb2
Armand Abasllari <armand.abasllari@gmail.com>
2026-02-06 10:22:39 +0100

    test

--- apps/api/app/db.py
+++ apps/api/app/db.py
@@ -7,7 +7,6 @@ if not DATABASE_URL:
     raise RuntimeError("DATABASE_URL not set")
 
 engine = create_engine(DATABASE_URL, pool_pre_ping=True)
-
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 
 class Base(DeclarativeBase):
